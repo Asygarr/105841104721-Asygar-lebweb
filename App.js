0c649d93bb29341633d14a90ffe73bdbd025663d
Asygarr <105841104721@student.unismuh.ac.id>
2024-06-28 10:43:56 +0800

    use font

--- App.js
+++ App.js
@@ -1,6 +1,65 @@
+import { View, Text } from "react-native";
 import React from "react";
-import AppNavigation from "./navigation/AppNavigation";
+import { useFonts } from "expo-font";
 
-export default function App() {
-  return <AppNavigation />;
-}
+const App = () => {
+  const [fontsLoaded] = useFonts({
+    MetroBlack: require("./assets/fonts/Metropolis-Black.otf"),
+    MetroBold: require("./assets/fonts/Metropolis-Bold.otf"),
+    MetroLight: require("./assets/fonts/Metropolis-Light.otf"),
+    MetroSemiBold: require("./assets/fonts/Metropolis-SemiBold.otf"),
+  });
+
+  if (!fontsLoaded) {
+    return <Text>Font tidak ditemukan</Text>;
+  }
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+
+      <Text
+        style={{
+          fontFamily: "MetroBlack",
+          fontSize: 24,
+        }}
+      >
+        Ini Font Metropolis Black
+      </Text>
+
+      <Text
+        style={{
+          fontFamily: "MetroBold",
+          fontSize: 24,
+        }}
+      >
+        Ini Font Metropolis Bold
+      </Text>
+
+      <Text
+        style={{
+          fontFamily: "MetroSemiBold",
+          fontSize: 24,
+        }}
+      >
+        Ini Font Metropolis SemiBold
+      </Text>
+
+      <Text
+        style={{
+          fontFamily: "MetroLight",
+          fontSize: 24,
+        }}
+      >
+        Ini Font Metropolis Light
+      </Text>
+    </View>
+  );
+};
+
+export default App;
